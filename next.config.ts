3931045fa8a3af9dae4ea7e4a68dc235d38bd797
Jan Hartje <github@janhartje.com>
2025-11-28 23:02:53 +0000

    cleanup

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-	output: 'export', // Das aktiviert den statischen Export
-	images: {
-		unoptimized: true, // Nötig für statischen Export
-	},
-};
-
-export default nextConfig;
